b871e81e10772c5c78d1154c573a946c82289443
Pradhap2895 <pradhapcrazy@gmail.com>
2022-02-09 19:35:24 +0530

    Service discovery

---
+++ Microservices/service-discovery.zip_expanded/online-shopping-eureka-server/src/main/java/com/project/onlineshoppingeurekaserver/OnlineShoppingEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.onlineshoppingeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingEurekaServerApplication.class, args);
+	}
+
+}
