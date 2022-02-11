66567cd565be512b92adc63cfb9da6e79617ac5b
unknown <nagasubbareddy949@gmail.com>
2022-02-11 10:49:11 +0530

    Payment UI

---
+++ payment-service/src/main/java/com/legato/paymentservice/controller/pageController.java
@@ -0,0 +1,13 @@
+package com.legato.paymentservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("firstapp")
+public class pageController {
+	
+	@GetMapping("/payment")
+	public String payment() {
+		return "payment";
+	}
+}
