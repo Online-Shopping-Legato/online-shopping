dd135fee24f11a46fd1f6a25a40baee47d846672
narasimha548 <narasimhaprasad548@gmail.com>
2022-02-15 10:20:39 +0530

    updated components

--- Online-Shopping-UI/OnlineShopping-Ui/src/app/fetch-items/fetch-items.component.ts
+++ Online-Shopping-UI/OnlineShopping-Ui/src/app/fetch-items/fetch-items.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ItemService } from '../item.service';
 import { ProductService } from '../product.service';
+import { SharedserviceService } from '../sharedservice.service';
 
 @Component({
   selector: 'app-fetch-items',
@@ -11,22 +12,24 @@ import { ProductService } from '../product.service';
 })
 export class FetchItemsComponent implements OnInit {
 
-  constructor(private _itemService: ItemService, private _builder:FormBuilder,
-    private _router:Router,private _productService: ProductService) { }
+  constructor(private _itemService: ItemService, private _builder: FormBuilder,
+    private _router: Router, private _productService: ProductService) { }
 
   ngOnInit(): void {
   }
 
-  details=this._builder.group({
-    productName : [''], price : ['']
+  details = this._builder.group({
+    productName: [''], price: ['']
   })
 
-  
+
   itemId: number = 0;
   res: any = undefined;
   products: any = undefined;
   msg: boolean = false;
   tab: boolean = false;
+
+  productArray: any = [{}];
 
   handleSubmit(): void {
 
@@ -69,10 +72,30 @@ export class FetchItemsComponent implements OnInit {
 
   }
 
-  handleCart() : void {
+  product = {}
 
-    console.log(this.details);
+  handleCart(productName: string, price: number, id: number): void {
+
+    this.product = {
+      'productName': productName,
+      'price': price,
+      'id': id
+    };
+
+    console.log(this.product)
+    const exists = this.productArray.filter((f: any) =>
+      f.productName === this.product
+    );
+    if (exists.length <= 0) {
+      console.log('insert:' + JSON.stringify(this.product));
+      //this._router.navigate(['/cart',id,productName,price]);
+      this._router.navigate(['/cart',JSON.stringify(this.product)]);
+    } else {
+      console.log('delete:' + JSON.stringify(this.product));
+      //delete this.productArray[this.product];
+    }
+
   }
 
- 
+
 }
